d34878e82f6258e3346b501d176da77e5bb0a783
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-09-20 12:10:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Hello World");
 
-console.log("HH   HH     eee     lll lll      ooo  ooo                       lll       dd !!!");
-console.log("HH HH HH    eee       lll lll       ooo ooo                          lll      dd !!!");
-console.log("HH   HH     eee    lll lll         ooo   ooo                      lll      dd !!!");
-console.log("HH   HH      eee      lll lll      ooo    ooo                        lll      dd !!!");
+console.log("HH   HH     eee eee eee      lll lll      llll lll          ooo ooo ooo       ww       ww          ww      lll       dd !!!");
+console.log("HH HH HH    eee eee  eee       lll lll      lll lll         ooo     ooo         ww    ww  ww     ww        lll      dd !!!");
+console.log("HH   HH     eee                  lll lll      lll lll       ooo     ooo           ww       ww   ww        lll      dd !!!");
+console.log("HH   HH      eee eee eee           lll lll       lll lll    ooo ooo ooo             ww       ww         lll      dd !!!");
